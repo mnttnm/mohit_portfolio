260e0c3f738765527dedf947cd57a8b62ccff5a7
mohit tater <mohittater.iiita@gmail.com>
2022-11-04 13:35:01 +0530

    Portfolio website project creation, textTheme defaults and color definition

---
+++ android/app/src/main/kotlin/com/example/mohit_portfolio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mohit_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/colors/colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+const secondaryGreenColor = Color.fromARGB(255, 60, 157, 147);
+const secondaryGreyColor = Color.fromARGB(255, 96, 123, 150);
+const secondaryBlueColor = Color.fromARGB(255, 77, 91, 206);
+const secondaryWhiteColor = Color.fromARGB(255, 255, 255, 255);
+
+const accentOrangeColor = Color.fromARGB(255, 254, 165, 95);
+const accentGreenColor = Color.fromARGB(255, 67, 217, 173);
+const accentBrownColor = Color.fromARGB(255, 233, 146, 135);
+const accentPurpleColor = Color.fromARGB(255, 201, 139, 223);
+
+const primaryColorDark = Color.fromARGB(255, 1, 8, 14);
+const primaryColor = Color.fromARGB(255, 1, 22, 39);
+const primaryColorLight = Color.fromARGB(255, 30, 45, 61);
+
+const gradientColorBlue = Color.fromARGB(255, 77, 91, 206);
+const gradientColorGreen = Color.fromARGB(255, 67, 217, 173);
+
+const darkGreyColor = Color.fromARGB(255, 30, 45, 61);
