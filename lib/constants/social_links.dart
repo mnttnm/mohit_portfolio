3cd4f032fa70b63d39a3ba17a410b2ebecd280a2
Mohit Tater <mohittater.iiita@gmail.com>
2024-08-30 21:41:42 +0530

    Update resume url to the latest

--- lib/constants/social_links.dart
+++ lib/constants/social_links.dart
@@ -3,4 +3,4 @@ const String linkedinProfileUrl = 'https://linkedin.com/in/tatermohit/';
 const String githubProfileUrl = 'https://github.com/mnttnm';
 const String instagramProfileUrl = 'https://instagram.com/_mohit_tater_/';
 const String resumeUrl =
-    'https://drive.google.com/file/d/1yBCIFDZkcUF6g1QiGFpyg0fyI-s1SRHI/view?usp=share_link';
+    'https://drive.google.com/file/d/1L-Vk-a2YIp4jyjkoknbMJpwTImcIp24C/view?usp=sharing';
